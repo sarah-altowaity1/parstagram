638a28810713e82cb36ca00341597feeed9a2b31
sarah-altowaity1 <sa5793@nyu.edu>
2022-10-13 19:30:02 -0400

    added progress files

---
+++ Parstagram/FeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FeedViewController.swift
+//  Parstagram
+//
+//  Created by Sarah Al-Towaity on 13/10/2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
